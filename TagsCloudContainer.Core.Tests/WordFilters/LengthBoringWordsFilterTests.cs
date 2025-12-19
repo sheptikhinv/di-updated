da24dca959db22231ff190464504d19d3f382b1f
shept <sheptikhinv@gmail.com>
2025-12-19 23:22:14 +0500

    add boring words filters tests

---
+++ TagsCloudContainer.Core.Tests/WordFilters/LengthBoringWordsFilterTests.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using TagsCloudContainer.Core.WordFilters;
+
+namespace TagsCloudContainer.Core.Tests.WordFilters;
+
+[TestFixture]
+public class LengthBoringWordsFilterTests
+{
+    [Test]
+    public void ExcludeBoringWords_RemovesShorterThan3()
+    {
+        var filter = new LengthBoringWordsFilter();
+        var input = new List<string> { "I", "am", "Ok", "nice", "WORLD" };
+
+        var result = filter.ExcludeBoringWords(input);
+
+        CollectionAssert.AreEquivalent(new[] { "nice", "world" }, result);
+    }
+
+    [Test]
+    public void GetBoringWords_ReturnsShortOnesOnly()
+    {
+        var filter = new LengthBoringWordsFilter();
+        var input = new List<string> { "a", "bb", "ccc", "dddd" };
+
+        var result = filter.GetBoringWords(input);
+
+        CollectionAssert.AreEquivalent(new[] { "a", "bb" }, result);
+    }
+}
