21acbbcecbf04072ef25cbe325067fda1ce29544
shept <sheptikhinv@gmail.com>
2025-12-21 11:49:15 +0500

    move DI logic to core

--- TagsCloudContainer.Cli/Program.cs
+++ TagsCloudContainer.Cli/Program.cs
@@ -1,11 +1,8 @@
-﻿using System.Drawing;
-using Autofac;
+﻿using Autofac;
 using CommandLine;
-using TagsCloudContainer.Core.CoordinateGenerators;
-using TagsCloudContainer.Core.FileReaders;
+using TagsCloudContainer.Core.DependencyInjection;
 using TagsCloudContainer.Core.Utils;
 using TagsCloudContainer.Core.Visualizators;
-using TagsCloudContainer.Core.WordFilters;
 
 namespace TagsCloudContainer.Cli;
 
@@ -25,7 +22,7 @@ class Program
         using var scope = container.BeginLifetimeScope();
         var wordProcessor = scope.Resolve<WordsProcessor>();
         var visualizator = scope.Resolve<IVisualizator>();
-        
+
         var count = wordProcessor.ProcessWords(options.FilePath);
         visualizator.DrawWordsToFile(count, options.OutputFilePath);
 
@@ -39,61 +36,14 @@ class Program
 
     private static IContainer BuildContainer(Options options)
     {
-        var builder = new ContainerBuilder();
-
-        builder.RegisterInstance(options).AsSelf().SingleInstance();
+        var builder = new ContainerBuilder()
+            .AddVisualizationOptions(options.BackgroundColor, options.TextColor, options.FontSize, options.ImageSize)
+            .AddFileReaders()
+            .AddWordsFilter(options.FilterFilePath)
+            .AddWordsProcessor()
+            .AddCoordinateGenerators(options.ImageSize, options.AngleStep)
+            .AddVisualizators();
         
-        builder.Register(c =>
-        {
-            var opts = c.Resolve<Options>();
-            var visualizationOptions = new VisualizationOptions
-            {
-                BackgroundColor = Color.FromName(opts.BackgroundColor).IsKnownColor
-                    ? Color.FromName(opts.BackgroundColor)
-                    : Color.Black,
-                FontColor = Color.FromName(opts.TextColor).IsKnownColor ? Color.FromName(opts.TextColor) : null,
-                FontSize = opts.FontSize,
-                ImageSize = opts.ImageSize
-            };
-            return visualizationOptions;
-        }).As<VisualizationOptions>();
-
-        builder.RegisterType<BasicFileReader>().As<IFileReader>();
-        builder.RegisterType<DocxFileReader>().As<IFileReader>();
-        
-        builder.Register(c =>
-        {
-            var readers = c.Resolve<IEnumerable<IFileReader>>();
-            return new FileReaderFactory(readers);
-        }).AsSelf().SingleInstance();
-        
-        builder.Register<IBoringWordsFilter>(c =>
-        {
-            var opts = c.Resolve<Options>();
-            if (!string.IsNullOrWhiteSpace(opts.FilterFilePath))
-            {
-                var factory = c.Resolve<FileReaderFactory>();
-                return new FileBoringWordsFilter(factory, opts.FilterFilePath);
-            }
-            return new LengthBoringWordsFilter();
-        }).As<IBoringWordsFilter>();
-
-        builder.Register(c =>
-        {
-            var readerFactory = c.Resolve<FileReaderFactory>();
-            var filter = c.Resolve<IBoringWordsFilter>();
-            return new WordsProcessor(readerFactory, filter);
-        }).AsSelf();
-
-        builder.Register(c =>
-        {
-            var opts = c.Resolve<Options>();
-            var center = new Point(opts.ImageSize / 2, opts.ImageSize / 2);
-            return new SpiralCoordinateGenerator(center, opts.AngleStep);
-        }).As<ICoordinateGenerator>();
-
-        builder.RegisterType<BasicVisualizator>().As<IVisualizator>();
-
         return builder.Build();
     }
 
---
+++ TagsCloudContainer.Core/DependencyInjection/AutofacDependencyInjection.cs
@@ -0,0 +1,95 @@
+﻿using System.Drawing;
+using Autofac;
+using TagsCloudContainer.Core.CoordinateGenerators;
+using TagsCloudContainer.Core.FileReaders;
+using TagsCloudContainer.Core.Utils;
+using TagsCloudContainer.Core.Visualizators;
+using TagsCloudContainer.Core.WordFilters;
+
+namespace TagsCloudContainer.Core.DependencyInjection;
+
+public static class AutofacDependencyInjection
+{
+    public static ContainerBuilder AddVisualizationOptions(this ContainerBuilder builder, string backgroundColor,
+        string textColor, float fontSize, int imageSize)
+    {
+        var visualizationOptions = new VisualizationOptions
+        {
+            BackgroundColor = Color.FromName(backgroundColor).IsKnownColor
+                ? Color.FromName(backgroundColor)
+                : Color.Black,
+            FontColor = Color.FromName(textColor).IsKnownColor ? Color.FromName(textColor) : null,
+            FontSize = fontSize,
+            ImageSize = imageSize
+        };
+
+        builder.RegisterInstance(visualizationOptions).AsSelf();
+        
+        return builder;
+    }
+
+    public static ContainerBuilder AddVisualizationOptions(this ContainerBuilder builder, VisualizationOptions options)
+    {
+        builder.RegisterInstance(options).AsSelf();
+
+        return builder;
+    }
+
+    public static ContainerBuilder AddFileReaders(this ContainerBuilder builder)
+    {
+        builder.RegisterType<BasicFileReader>().As<IFileReader>();
+        builder.RegisterType<DocxFileReader>().As<IFileReader>();
+
+        builder.Register(c =>
+        {
+            var readers = c.Resolve<IEnumerable<IFileReader>>();
+            return new FileReaderFactory(readers);
+        }).AsSelf().SingleInstance();
+
+        return builder;
+    }
+
+    public static ContainerBuilder AddWordsFilter(this ContainerBuilder builder, string? filterFilePath)
+    {
+        builder.Register<IBoringWordsFilter>(c =>
+        {
+            if (string.IsNullOrWhiteSpace(filterFilePath))
+                return new LengthBoringWordsFilter();
+            var factory = c.Resolve<FileReaderFactory>();
+            return new FileBoringWordsFilter(factory, filterFilePath);
+        }).As<IBoringWordsFilter>();
+
+        return builder;
+    }
+
+    public static ContainerBuilder AddWordsProcessor(this ContainerBuilder builder)
+    {
+        builder.Register(c =>
+        {
+            var readerFactory = c.Resolve<FileReaderFactory>();
+            var filter = c.Resolve<IBoringWordsFilter>();
+            return new WordsProcessor(readerFactory, filter);
+        }).AsSelf();
+
+        return builder;
+    }
+
+    public static ContainerBuilder AddCoordinateGenerators(this ContainerBuilder builder, int imageSize = 2048,
+        double angleStep = 0.1f)
+    {
+        builder.Register(c =>
+        {
+            var center = new Point(imageSize / 2, imageSize / 2);
+            return new SpiralCoordinateGenerator(center, angleStep);
+        }).As<ICoordinateGenerator>();
+
+        return builder;
+    }
+
+    public static ContainerBuilder AddVisualizators(this ContainerBuilder builder)
+    {
+        builder.RegisterType<BasicVisualizator>().As<IVisualizator>();
+
+        return builder;
+    }
+}
