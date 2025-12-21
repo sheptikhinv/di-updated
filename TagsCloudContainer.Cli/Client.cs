1bf93e2e7ae7a5086fdc85aeb26046e3d63db3ec
shept <sheptikhinv@gmail.com>
2025-12-21 13:50:16 +0500

    most clean Program.cs possible

---
+++ TagsCloudContainer.Cli/Client.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using TagsCloudContainer.Core.Utils;
+using TagsCloudContainer.Core.Visualizators;
+
+namespace TagsCloudContainer.Cli;
+
+public class Client
+{
+    public void Run(Options options)
+    {
+        var container = Startup.ConfigureServices(options);
+
+        using var scope = container.BeginLifetimeScope();
+        var wordProcessor = scope.Resolve<WordsProcessor>();
+        var visualizator = scope.Resolve<IVisualizator>();
+
+        var count = wordProcessor.ProcessWords(options.FilePath);
+        visualizator.DrawWordsToFile(count, options.OutputFilePath);
+
+        foreach (var pair in count.OrderByDescending(pair => pair.Value))
+        {
+            Console.WriteLine($"{pair.Key}: {pair.Value}");
+        }
+
+        Console.WriteLine($"Visualization saved to file {options.OutputFilePath}");
+    }
+}
--- TagsCloudContainer.Cli/Program.cs
+++ TagsCloudContainer.Cli/Program.cs
@@ -9,28 +9,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        var client = new Client();
         Parser.Default.ParseArguments<Options>(args)
-            .WithParsed(Run)
+            .WithParsed(client.Run)
             .WithNotParsed(HandleParseErrors);
-    }
-
-    private static void Run(Options options)
-    {
-        var container = Startup.ConfigureServices(options);
-
-        using var scope = container.BeginLifetimeScope();
-        var wordProcessor = scope.Resolve<WordsProcessor>();
-        var visualizator = scope.Resolve<IVisualizator>();
-
-        var count = wordProcessor.ProcessWords(options.FilePath);
-        visualizator.DrawWordsToFile(count, options.OutputFilePath);
-
-        foreach (var pair in count.OrderByDescending(pair => pair.Value))
-        {
-            Console.WriteLine($"{pair.Key}: {pair.Value}");
-        }
-
-        Console.WriteLine($"Visualization saved to file {options.OutputFilePath}");
     }
 
     private static void HandleParseErrors(IEnumerable<Error> errors)
